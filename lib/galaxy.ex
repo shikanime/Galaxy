e662bdd5e1b7477746c048b46e56da526a9f826e
Shikanime Deva <deva.shikanime@protonmail.com>
2020-01-08 21:05:29 +0100

    Remove hello world code

--- lib/galaxy.ex
+++ lib/galaxy.ex
@@ -1,18 +1,2 @@
 defmodule Galaxy do
-  @moduledoc """
-  Documentation for Galaxy.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Galaxy.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
