0e634b1b650629df2f3336c8dbecfcbec9a005b0
Shikanime Deva <deva.shikanime@protonmail.com>
2020-01-08 21:43:50 +0100

    Downgrade Elixir to 1.9

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Galaxy.MixProject do
     [
       app: :galaxy,
       version: "0.1.0",
-      elixir: "~> 1.10-dev",
+      elixir: "~> 1.9.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
